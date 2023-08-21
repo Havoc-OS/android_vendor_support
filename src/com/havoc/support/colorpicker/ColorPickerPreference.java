567edc76fa3274b2745aff9066d127159a9e8678
Anushek Prasal <anushekprasal@gmail.com>
2023-08-21 18:35:36 +0530

    support: Update ColorPicker layout
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/com/havoc/support/colorpicker/ColorPickerPreference.java
+++ src/com/havoc/support/colorpicker/ColorPickerPreference.java
@@ -86,7 +86,7 @@ public class ColorPickerPreference extends Preference implements
     public ColorPickerPreference(Context context, AttributeSet attrs, int defStyleAttr,
             int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        setLayoutResource(R.layout.preference_material_settings);
+        setLayoutResource(R.layout.preference_material_color_picker_settings);
         init(context, attrs);
 
         mContext = context;
